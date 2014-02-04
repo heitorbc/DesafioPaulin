60dd8488903c5b1870dbffdd5ce387dbd01fad51
20121BSI0082 <20121BSI0082@SR6713.cefetes.br>
2014-02-04 14:25:53 -0200

    Primeiro Commit

---
+++ src/br/ifes/poo1/app/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ifes.poo1.app;
+
+/**
+ *
+ * @author 20121BSI0082
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
